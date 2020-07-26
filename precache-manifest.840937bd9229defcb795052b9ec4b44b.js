7fa2c2b04f900871545dcf970484e850a49ac07e
MiroslavMashivskyi <62468496+MiroslavMashivskyi@users.noreply.github.com>
2020-07-26 09:58:08 +0300

    Updates

---
+++ precache-manifest.840937bd9229defcb795052b9ec4b44b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0449f6dd82f0283879d114d45c98eb8",
+    "url": "/user_survey/index.html"
+  },
+  {
+    "revision": "4e0e7f5370c5ddce5158",
+    "url": "/user_survey/static/css/main.145768df.chunk.css"
+  },
+  {
+    "revision": "ff7176b88dc1a3b86451",
+    "url": "/user_survey/static/js/2.9ca63fac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/user_survey/static/js/2.9ca63fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0e7f5370c5ddce5158",
+    "url": "/user_survey/static/js/main.a28d446b.chunk.js"
+  },
+  {
+    "revision": "72744ab3e4f610deec64",
+    "url": "/user_survey/static/js/runtime-main.c5560c3d.js"
+  },
+  {
+    "revision": "5e44a2a57555524e23ce4938cccf7610",
+    "url": "/user_survey/static/media/Nature_Beach_Golden_Beach.5e44a2a5.jpg"
+  }
+]);
